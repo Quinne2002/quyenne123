710fdd8edcbe6c894c015a5fd4ed3ba099c1fca7
Quinne2002 <122085456+Quinne2002@users.noreply.github.com>
2023-04-22 23:56:00 +0700

    update4

--- src/Board.js
+++ src/Board.js
@@ -11,7 +11,7 @@ export default function Board({ squares, onClick }) {
   };
 
   return (
-    <div className="options">
+    <div>
       <div className="board-row">{renderSquares([0, 1, 2])}</div>
       <div className="board-row">{renderSquares([3, 4, 5])}</div>
       <div className="board-row">{renderSquares([6, 7, 8])}</div>
